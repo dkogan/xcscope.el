bdad2f462619fce151b2b171a390bc466c534ff0
Dima Kogan <dima@secretsauce.net>
2013-09-05 11:17:06 -0700

    corrected some comments

--- xcscope.el
+++ xcscope.el
@@ -100,10 +100,10 @@
 ;;	(define-key global-map [(control f7)]
 ;;	  'cscope-find-global-definition-no-prompting)
 ;;	(define-key global-map [(control f8)]  'cscope-pop-mark)
-;;	(define-key global-map [(control f9)]  'cscope-next-symbol)
-;;	(define-key global-map [(control f10)] 'cscope-next-file)
-;;	(define-key global-map [(control f11)] 'cscope-prev-symbol)
-;;	(define-key global-map [(control f12)] 'cscope-prev-file)
+;;	(define-key global-map [(control f9)]  'cscope-history-forward-line)
+;;	(define-key global-map [(control f10)] 'cscope-history-forward-file)
+;;	(define-key global-map [(control f11)] 'cscope-history-backward-line)
+;;	(define-key global-map [(control f12)] 'cscope-history-backward-file)
 ;;      (define-key global-map [(meta f9)]  'cscope-display-buffer)
 ;;      (define-key global-map [(meta f10)] 'cscope-display-buffer-toggle)
 ;;
